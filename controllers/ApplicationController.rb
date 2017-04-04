a382c69e8b35b238d85f99701533ca9f791578c6
Terry Lippincott <tlippincott25@gmail.com>
2017-04-04 16:30:03 -0500

    added public folder and gitignore file

--- controllers/ApplicationController.rb
+++ controllers/ApplicationController.rb
@@ -10,6 +10,6 @@ class ApplicationController < Sinatra::Base
     )
 
     set :views, File.expand_path('../../views', __FILE__)
-    #set :public_dir, File.expand_path('../../public', __FILE__)
+    set :public_dir, File.expand_path('../../public', __FILE__)
 
 end
