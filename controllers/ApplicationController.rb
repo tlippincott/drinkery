8ebfc99956818d07d5a6c69549fb7677b6ebddb8
Terry Lippincott <tlippincott25@gmail.com>
2017-04-07 10:30:04 -0500

    added 404 page

--- controllers/ApplicationController.rb
+++ controllers/ApplicationController.rb
@@ -12,4 +12,8 @@ class ApplicationController < Sinatra::Base
     set :views, File.expand_path('../../views', __FILE__)
     set :public_dir, File.expand_path('../../public', __FILE__)
 
+    not_found do
+      erb :not_found
+    end
+
 end
